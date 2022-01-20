786db01fb05b6a01ecac598a95743bcbce9d96bb
Phil Splettstoeszer <stoeszer09@gmail.com>
2022-01-19 19:04:30 -0600

    items can add to inventory

--- index.js
+++ index.js
@@ -10,6 +10,16 @@ function newImage(url, left, bottom){
 
 function newItem(url, left, bottom){
     let item = newImage(url, left, bottom)
+    item.addEventListener('click', function() {
+        addItemToInventory(item, url)
+    })
+}
+
+function addItemToInventory (item, url) {
+    item.remove()
+    let inventoryItem = document.createElement('img')
+    inventoryItem.src = url
+    inventory.append(inventoryItem)
 }
 
 function newInventory(){
@@ -26,9 +36,11 @@ function newInventory(){
     inventory.style.border = '2px solid black'
     inventory.style.backgroundColor = 'brown'
     document.body.append(inventory)
+    return inventory
 }
 
-newInventory()
+
+let inventory = newInventory()
 newImage('assets/green-character.gif', 100, 250)
 newImage('assets/tree.png', 200, 450)
 newImage('assets/pillar.png', 350, 250)
